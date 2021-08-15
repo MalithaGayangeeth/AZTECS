16b7bde420c9fd08343a69f19a418cc6b703242f
MalithaGayangeeth <It20043032@my.sliit.lk>
2021-08-15 16:35:50 +0530

    initial commit

---
+++ app/src/main/java/com/example/aztecs/TotalBill.java
@@ -0,0 +1,14 @@
+package com.example.aztecs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TotalBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total_bill);
+    }
+}
