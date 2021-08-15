8d650430d1690d503836e2c9e66d27cc6ed84dda
Asus <it20016302@my.sliit.lk>
2021-08-15 09:49:10 -0700

    commit message gose here

---
+++ app/src/main/java/com/example/aztecs/MainActivity04.java
@@ -0,0 +1,14 @@
+package com.example.aztecs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main04);
+    }
+}
---
+++ app/src/main/java/com/example/aztecs/MainActivity06.java
@@ -0,0 +1,14 @@
+package com.example.aztecs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity06 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main06);
+    }
+}
