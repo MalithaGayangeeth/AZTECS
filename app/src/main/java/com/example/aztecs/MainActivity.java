53f75cbd51cb7c319931cdd3e9e0f4024353e1f6
Asus <it20016302@my.sliit.lk>
2021-08-17 00:49:34 -0700

    commit gose here.

--- app/src/main/java/com/example/aztecs/MainActivity.java
+++ app/src/main/java/com/example/aztecs/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.aztecs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton imageButton13;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton13 = (ImageButton) findViewById(R.id.imageButton13);
+
+        imageButton13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openMainActivity04();
+            }
+
+            public void openMainActivity04() {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, MainActivity04.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/aztecs/MainActivity04.java
+++ app/src/main/java/com/example/aztecs/MainActivity04.java
@@ -2,13 +2,47 @@ package com.example.aztecs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity04 extends AppCompatActivity {
-
+private Button button4;
+private Button button10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main04);
+
+        button4 = (Button) findViewById(R.id.button4);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openMainActivity05();
+            }
+
+            public void openMainActivity05() {
+                Intent intentLoadNewActivity = new Intent(MainActivity04.this, MainActivity05.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+        button10 = (Button) findViewById(R.id.button10);
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openMainActivity06();
+            }
+
+            public void openMainActivity06() {
+                Intent intentLoadNewActivity = new Intent(MainActivity04.this, MainActivity06.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
