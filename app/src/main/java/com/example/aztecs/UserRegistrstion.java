80ff6cab2c05ea4b045e6335cba209accf13b830
MalithaGayangeeth <It20043032@my.sliit.lk>
2021-08-17 12:55:26 +0530

    initial commit

--- app/src/main/java/com/example/aztecs/UserRegistrstion.java
+++ app/src/main/java/com/example/aztecs/UserRegistrstion.java
@@ -2,13 +2,27 @@ package com.example.aztecs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserRegistrstion extends AppCompatActivity {
-
+Button button9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registrstion);
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUserProfile();
+            }
+            public void openUserProfile(){
+                Intent intent= new Intent(UserRegistrstion.this,UserProfile.class);
+                startActivity(intent);}
+
+        });
     }
 }
